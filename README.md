2cfeeea54ceedb2ff829cb42e6ec8104280f9c22
lucky <luckylibora@gmail.com>
2017-08-13 22:09:50 +0300

    Support for inner objects and primitives

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # obj-emitter
-Simple event emitter which use object instead of strings
+Emit anything you want. If object is suitable then object will be handled by callback.
+Optionally you can use this way to emit booleans, strings, numbers, symbols, arrays  
 
 # Example
 
@@ -8,8 +9,14 @@ Simple event emitter which use object instead of strings
 const objEmitter = new ObjEmitter();
 objEmitter.on({a:1, b:2}, obj => console.log('Emits', obj));
 
-obj.emit({a:1, b:2});       //call handler
-obj.emit({a:1, b:2, c:3});  //call handler
-obj.emit({a:2, b:5});       //do not call handler
+objEmitter.emit({a:1, b:2});       //Emits {a:1, b:2}
+objEmitter.emit({a:1, b:2, c:3});  //Emits {a:1, b:2, c:3}
+objEmitter.emit({a:2, b:5});       //do not call handler
+
+const cb = o => console.log(o);
+objEmitter.on({x: 1, y: {z: 'test'}, cb);
+objEmitter.removeListener({x: 1, y: {z: 'test'}, cb);  //removing listener
+
+objEmitter.removeAllListeners({a:1, b:2}); //remove all listeners of this object pattern
 
 ```
--- test/ObjectEmitterTest.js
+++ test/ObjectEmitterTest.js
@@ -1,11 +1,13 @@
-require('chai').should();
-const ObjEmitter = require('../');
+const chai          = require('chai');
+const ObjectEmitter = require('../index');
+
+chai.should();
 
 describe('ObjEmitter', () => {
 
     let oe;
 
-    beforeEach(() => oe = new ObjEmitter());
+    beforeEach(() => oe = new ObjectEmitter());
 
     describe('#emit', () => {
 
@@ -45,32 +47,116 @@ describe('ObjEmitter', () => {
             oe.emit({a: 1, b: 2});
         });
 
+        it('inner object', cb => {
+            oe.on({a: 1, b: {c: 1}}, obj => {
+                obj.should.deep.equal({a: 1, b: {c: 1, d: 2}});
+                cb();
+            });
+            oe.emit({a: 1, b: {c: 1, d: 2}});
+        });
+
+        it('not valid inner object', cb => {
+            oe.on({a: 1, b: {c: 1}}, obj => cb(new Error()));
+            oe.emit({a: 1, b: {d: 2}});
+            cb();
+        });
+
+        it('array check', cb => {
+            oe.on([1, 2, 3], obj => {
+                obj.should.deep.equal([1, 2, 3, 4]);
+                cb();
+            });
+            oe.emit([1, 2, 3, 4]);
+        });
+
+        it('not valid array check', cb => {
+            oe.on([1, 2, 3], obj => cb(new Error()));
+            oe.emit([1, 2, 4]);
+            cb();
+        });
+
+        it('string check', cb => {
+            oe.on('test', obj => {
+                obj.should.equal('test');
+                cb();
+            });
+            oe.emit('test');
+        });
+
+        it('not valid string check', cb => {
+            oe.on('test', () => cb(new Error()));
+            oe.emit('test123');
+            cb();
+        });
+
+        it('number check', cb => {
+            oe.on(123, o => {
+                o.should.equal(123);
+                cb();
+            });
+            oe.emit(123);
+        });
+
+        it('not valid number check', cb => {
+            oe.on(123, o => cb(new Error()));
+            oe.emit(1234);
+            cb();
+        });
+
+        it('boolean check', cb => {
+            oe.on(true, o => {
+                o.should.equal(true);
+                cb();
+            });
+            oe.emit(true);
+        });
+
+        it('not valid boolean', cb => {
+            oe.on(true, () => cb(new Error()));
+            oe.emit(false);
+            cb();
+        });
+
+        it('undefined', cb => {
+            oe.on(undefined, o => {
+                chai.expect(o).to.equal(undefined);
+                cb();
+            });
+            oe.emit(undefined);
+        });
+
+        it('null', cb => {
+            oe.on(null, o => {
+                chai.expect(o).to.equal(null);
+                cb();
+            });
+            oe.emit(null);
+        });
+
     });
 
     describe('#once', () => {
 
-        it('basic test', () => {
-            let counter = 0;
-            oe.once({a: 1}, () => counter++);
-            oe.emit({a: 1});
-            oe.emit({a: 1});
-            counter.should.equal(1);
-        });
-
-        it('equal objects', cb => {
+        it('equal objects', () => {
+            let count = 0;
             oe.once({a: 1, b: 2}, obj => {
                 obj.should.deep.equal({a: 1, b: 2});
-                cb();
+                count++;
             });
             oe.emit({a: 1, b: 2});
+            oe.emit({a: 1, b: 2});
+            count.should.equal(1);
         });
 
-        it('emit object with additional fields', cb => {
+        it('emit object with additional fields', () => {
+            let count = 0;
             oe.once({a: 1, b: 2}, obj => {
                 obj.should.deep.equal({a: 1, b: 2, c: 3});
-                cb();
+                count++;
             });
             oe.emit({a: 1, b: 2, c: 3});
+            oe.emit({a: 1, b: 2, c: 3});
+            count.should.equal(1);
         });
 
         it('not equal values', cb => {
@@ -85,12 +171,122 @@ describe('ObjEmitter', () => {
             cb();
         });
 
-        it('on empty object', cb => {
+        it('on empty object', () => {
+            let count = 0;
             oe.once({}, obj => {
                 obj.should.deep.equal({a: 1, b: 2});
-                cb();
+                count++;
             });
             oe.emit({a: 1, b: 2});
+            oe.emit({a: 1, b: 2});
+            count.should.equal(1);
+        });
+
+        it('inner object', () => {
+            let count = 0;
+            oe.once({a: 1, b: {c: 1}}, obj => {
+                obj.should.deep.equal({a: 1, b: {c: 1, d: 2}});
+                count++;
+            });
+            oe.emit({a: 1, b: {c: 1, d: 2}});
+            oe.emit({a: 1, b: {c: 1, d: 2}});
+            count.should.equal(1);
+        });
+
+        it('not valid inner object', cb => {
+            oe.once({a: 1, b: {c: 1}}, obj => cb(new Error()));
+            oe.emit({a: 1, b: {d: 2}});
+            cb();
+        });
+
+        it('array check', () => {
+            let count = 0;
+            oe.once([1, 2, 3], obj => {
+                obj.should.deep.equal([1, 2, 3, 4]);
+                count++;
+            });
+            oe.emit([1, 2, 3, 4]);
+            oe.emit([1, 2, 3, 4]);
+            count.should.equal(1);
+        });
+
+        it('not valid array check', cb => {
+            oe.once([1, 2, 3], obj => cb(new Error()));
+            oe.emit([1, 2, 4]);
+            cb();
+        });
+
+        it('string check', () => {
+            let count = 0;
+            oe.once('test', obj => {
+                obj.should.equal('test');
+                count++;
+            });
+            oe.emit('test');
+            oe.emit('test');
+            count.should.equal(1);
+        });
+
+        it('not valid string check', cb => {
+            oe.once('test', () => cb(new Error()));
+            oe.emit('test123');
+            cb();
+        });
+
+        it('number check', () => {
+            let count = 0;
+            oe.once(123, o => {
+                o.should.equal(123);
+                count++;
+            });
+            oe.emit(123);
+            oe.emit(123);
+            count.should.equal(1);
+        });
+
+        it('not valid number check', cb => {
+            oe.once(123, o => cb(new Error()));
+            oe.emit(1234);
+            cb();
+        });
+
+        it('boolean check', () => {
+            let count = 0;
+            oe.once(true, o => {
+                o.should.equal(true);
+                count++;
+            });
+            oe.emit(true);
+            oe.emit(true);
+            count.should.equal(1);
+        });
+
+        it('not valid boolean', cb => {
+            oe.once(true, () => cb(new Error()));
+            oe.emit(false);
+            cb();
+        });
+
+        it('undefined', () => {
+            let count = 0;
+            oe.once(undefined, o => {
+                chai.expect(o).to.equal(undefined);
+                count++;
+            });
+            oe.emit(undefined);
+            oe.emit(undefined);
+            count.should.equal(1);
+        });
+
+        it('null', () => {
+            let count = 0;
+            oe.once(null, o => {
+                chai.expect(o).to.equal(null);
+                count++;
+            });
+            oe.emit(null);
+            oe.emit(null);
+            count.should.equal(1);
         });
 
     });
@@ -128,6 +324,36 @@ describe('ObjEmitter', () => {
             cb()
         });
 
+        it('inner objects', cb => {
+            const fn = obj => cb(new Error('Should not call callback'));
+            oe.on({a: 1, b: 2, c: {d: 1, e: 2}}, fn);
+            oe.removeListener({a: 1, b: 2, c: {d: 1, e: 2}}, fn);
+            oe.emit({a: 1, b: 2, c: {d: 1, e: 2}, z: {}});
+            cb();
+        });
+
+        it('primitive', cb => {
+            const fn = () => cb(new Error());
+            oe.on('test', fn);
+            oe.removeListener('test', fn);
+            cb();
+        });
+
+
+        it('undefined', cb => {
+            const fn = () => cb(new Error());
+            oe.on(undefined, fn);
+            oe.removeListener(undefined, fn);
+            cb();
+        });
+
+        it('null', cb => {
+            const fn = () => cb(new Error());
+            oe.on(null, fn);
+            oe.removeListener(null, fn);
+            cb();
+        });
+
     });
 
     describe('#removeAllListeners', () => {
@@ -138,6 +364,33 @@ describe('ObjEmitter', () => {
             oe.removeAllListeners({a: 1, b: 2});
             oe.emit({a: 1, b: 2});
             cb();
+        });
+
+        it('primitive', cb => {
+            oe.on('test', () => cb(new Error('Should not call callback')));
+            oe.on('test', () => cb(new Error('Should not call callback')));
+            oe.removeAllListeners('test');
+            oe.emit('test');
+            cb();
+
+        });
+
+        it('null', cb => {
+            oe.on(null, () => cb(new Error('Should not call callback')));
+            oe.on(null, () => cb(new Error('Should not call callback')));
+            oe.removeAllListeners(null);
+            oe.emit(null);
+            cb();
+
+        });
+
+        it('undefined', cb => {
+            oe.on(undefined, () => cb(new Error('Should not call callback')));
+            oe.on(undefined, () => cb(new Error('Should not call callback')));
+            oe.removeAllListeners(undefined);
+            oe.emit(undefined);
+            cb();
+
         });
 
     });
